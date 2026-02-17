2cb29df183092d07d22bbe0bfcff79f3b8fb56b8
jklagstad <jklagstad1@my.wctc.edu>
2026-02-16 18:35:15 -0600

    accidentally submitted the wrong assignment

--- discount.js
+++ discount.js
@@ -1,22 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
   const toastEl = document.getElementById('liveToast');
-  const toast = bootstrap.Toast.getOrCreateInstance(toastEl, { autohide: false });
 
-  document.getElementById('discount-row').addEventListener('click', function(e){
-    if (e.target.classList.contains('discount')) {
+  // Prevent autohide
+  const toast = new bootstrap.Toast(toastEl, {
+    autohide: false
+  });
+
+  document.getElementById('discount-row').addEventListener('click', function (e) {
+
+    if (e.target.closest('.discount')) {
       e.preventDefault();
 
-      document.getElementById('product').innerHTML = e.target.dataset['product'];
-      document.getElementById('discountTitle').innerHTML = e.target.dataset['title'];
-      document.getElementById('discountCode').innerHTML =
-        "Discount Code: " + e.target.dataset['code'];
+      const link = e.target.closest('.discount');
+
+      // Populate toast from data attributes
+      document.getElementById('product').textContent =
+        link.dataset.product;
+
+      document.getElementById('discountTitle').textContent =
+        link.dataset.title;
+
+      document.getElementById('discountCode').textContent =
+        "Discount Code: " + link.dataset.code;
 
       toast.show();
     }
   });
 
-  document.addEventListener('keydown', function(e) {
+  // Close toast on Escape key
+  document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape') {
       toast.hide();
     }
